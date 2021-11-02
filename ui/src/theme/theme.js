413f63eb054b90e20dc77c170b3990c379985d15
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-02 14:43:24 -0300

    feat(ui): create the color scheme on theme

--- ui/src/theme/theme.js
+++ ui/src/theme/theme.js
@@ -1,6 +1,23 @@
 import { createTheme } from '@mui/material/styles';
 import { ptBR } from '@mui/material/locale';
 
-const theme = createTheme({}, ptBR);
+const theme = createTheme(
+  {
+    palette: {
+      primary: {
+        main: '#00fa9a',
+        dark: '#127358',
+      },
+      secondary: {
+        main: '##fed500',
+        dark: '##e4c000',
+      },
+      common: {
+        black: '#191a1a',
+      },
+    },
+  },
+  ptBR
+);
 
 export default theme;
