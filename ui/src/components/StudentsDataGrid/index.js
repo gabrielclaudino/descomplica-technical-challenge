5a2ae0079bdae8ee3ec6321bb3a9ccad58085b2a
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-05 02:46:32 -0300

    feat(ui, student-data-grid): improve the layout and add parameters to handle the user input

--- ui/src/components/StudentsDataGrid/index.js
+++ ui/src/components/StudentsDataGrid/index.js
@@ -4,21 +4,44 @@ import { DataGrid } from '@mui/x-data-grid';
 import { styled } from '@mui/system';
 
 const Wrapper = styled('div')(({ theme }) => ({
-  'padding-top': '2rem',
-  height: '80vh',
+  paddingTop: '1rem',
+  height: '70vh',
+  paddingBottom: '2rem',
 }));
 
-const StudentsDataGrid = ({ rows, columns }) => {
+const StudentsDataGrid = ({
+  rows,
+  columns,
+  loading,
+  onFilterChange,
+  onEditCommit,
+}) => {
   return (
     <Wrapper>
-      <DataGrid rows={rows} columns={columns} />
+      <DataGrid
+        pageSize={25}
+        loading={loading}
+        rows={rows}
+        columns={columns}
+        filterMode="server"
+        editMode="row"
+        onFilterModelChange={onFilterChange}
+        onRowEditCommit={onEditCommit}
+      />
     </Wrapper>
   );
+};
+
+StudentsDataGrid.defaultProps = {
+  loading: false,
 };
 
 StudentsDataGrid.propTypes = {
   rows: PropTypes.arrayOf(Object),
   columns: PropTypes.arrayOf(Object),
+  loading: PropTypes.bool,
+  onFilterChange: PropTypes.func,
+  onEditCommit: PropTypes.func,
 };
 
 export default StudentsDataGrid;
