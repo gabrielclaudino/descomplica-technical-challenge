2105d5dc145ed8be065ae959b594f0aff359c65b
Gabriel Claudino <gabrielhc@protonmail.com>
2021-11-06 20:13:46 -0300

    feat(searchbar): remove elements and add a sx prop

--- ui/src/components/SearchBar/index.js
+++ ui/src/components/SearchBar/index.js
@@ -1,48 +1,37 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { IconButton, InputAdornment, TextField } from '@mui/material';
+import { IconButton, TextField } from '@mui/material';
 import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
-import SearchIcon from '@mui/icons-material/Search';
 
-const SearchBar = ({ value, setValue, onBack }) => {
+const SearchBar = ({ value, setValue, sx }) => {
   return (
-    <>
-      <TextField
-        placeholder="Pesquisar"
-        type="text"
-        variant="outlined"
-        fullWidth
-        size="small"
-        onChange={(e) => setValue(e.target.value)}
-        value={value}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <ArrowBackIcon aria-label="voltar" onClick={() => onBack()} />
-            </InputAdornment>
-          ),
-          endAdornment: value && (
-            <IconButton
-              aria-label="limpa texto da pesquisa"
-              onClick={() => setValue('')}
-            >
-              <CancelRoundedIcon />
-            </IconButton>
-          ),
-        }}
-      />
-      <IconButton sx={{ p: '10px' }} aria-label="pesquisar">
-        <SearchIcon />
-      </IconButton>
-    </>
+    <TextField
+      placeholder="Pesquisar"
+      type="text"
+      variant="outlined"
+      fullWidth
+      size="small"
+      sx={sx}
+      onChange={(e) => setValue(e.target.value)}
+      value={value}
+      InputProps={{
+        endAdornment: value && (
+          <IconButton
+            aria-label="limpa texto da pesquisa"
+            onClick={() => setValue('')}
+          >
+            <CancelRoundedIcon />
+          </IconButton>
+        ),
+      }}
+    />
   );
 };
 
 SearchBar.propTypes = {
   value: PropTypes.string,
   setValue: PropTypes.func,
-  onBack: PropTypes.func,
+  sx: PropTypes.object,
 };
 
 export default SearchBar;
